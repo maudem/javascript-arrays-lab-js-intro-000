02533e95e842832af7283dbe9cdaf6922b5dbd0b
maude sos <mualth@gmail.com>
2017-09-16 00:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 var destructivelyAppendKitten = name => {
-  kittens.unshift(name)
+  kittens.push(name)
   return kittens
 }
